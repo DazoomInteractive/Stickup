f4d058ff1aaf9ff9f82c7d783ad63109d0df397c
DazoomInteractive <dazoomgamecreator@gmail.com>
2026-07-28 03:15:00 +0500

    Added gamejolt-export.zip

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './',
+  build: {
+    assetsDir: '',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'index.js',
+        chunkFileNames: 'index.js',
+        assetFileNames: 'index.[ext]',
+      },
+    },
+  },
 })
